c67828c702a0106776be1c01c387238ccabc24f7
fhsiao <frank.hsiao@hotmail.com>
2018-07-08 18:47:14 -0600

    PT-1: Add SIP UA

--- pjapi/src/main/pjapi/jni-include/common.h
+++ pjapi/src/main/pjapi/jni-include/common.h
@@ -26,3 +26,10 @@
 #endif
 
 
+void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id, pjsip_rx_data *rdata);
+void on_call_state(pjsua_call_id call_id, pjsip_event *e);
+void on_call_media_state(pjsua_call_id call_id);
+void error_exit(const char *title, pj_status_t status);
+
+
+
--- pjapi/src/main/pjapi/pjApi.c
+++ pjapi/src/main/pjapi/pjApi.c
@@ -3,9 +3,15 @@
 #define __PJAPI__
 
 #include "jni-include/common.h"
+#include <pjsua-lib/pjsua.h>
 
-pjsua_transport_id udp_id;
-int engineInitialized;
+#ifndef TAG
+#define TAG "pjApi.c"
+#endif
+
+#define SIP_DOMAIN "example.com"
+#define SIP_USER "alice"
+#define SIP_PASSWD "secret"
 
 JNIEXPORT jstring Java_com_freetalk_pjinterface_PJ_helloJNI(JNIEnv* env, jobject obj)
 {
@@ -14,70 +20,67 @@ JNIEXPORT jstring Java_com_freetalk_pjinterface_PJ_helloJNI(JNIEnv* env, jobject
 
 JNIEXPORT jint Java_com_freetalk_pjinterface_PJ_createSipEngineJNI(JNIEnv* env, jobject obj)
 {
-    pjsua_config ua_cfg;
-    pjsua_logging_config log_cfg;
-    pjsua_media_config media_cfg;
-    pjsua_transport_config transport_cfg;
+
+    pjsua_acc_id acc_id;
     pj_status_t status;
-    
+
+/* Get JavaVM! */
     int jstatus = (*env)->GetJavaVM(env, &g_vm);
     if (jstatus != 0) {
         __android_log_print(ANDROID_LOG_INFO, TAG, "Failed to get pointer to Java Virtual Machine");
         return jstatus;
     }
 
-    // Must create pjsua before anything else!
+/* Create pjsua first! */
     status = pjsua_create();
-    if (status != PJ_SUCCESS) {
-        pjsua_perror("pjApi.c", "Error initializing pjsua", status);
-        return status;
-    }
+    if (status != PJ_SUCCESS) error_exit("Error in pjsua_create()", status);
 
-    // Create a memory pool to be used by JNI services calling pj functions.
-    poolJNI = pjsua_pool_create("vphonePool", 4096, 4096);
-    if (poolJNI == NULL)
+    /* Validating SIP URL */
+    status = pjsua_verify_url(SIP_DOMAIN);
+    if (status != PJ_SUCCESS) error_exit("Invalid URL in argv", status);
+
+/* Init pjsua */
     {
-        return status;    // error on creating our memory pool.
+        pjsua_config cfg;
+        pjsua_logging_config log_cfg;
+        pjsua_config_default(&cfg);
+        cfg.cb.on_incoming_call = &on_incoming_call;
+        cfg.cb.on_call_media_state = &on_call_media_state;
+        cfg.cb.on_call_state = &on_call_state;
+        pjsua_logging_config_default(&log_cfg);
+        log_cfg.console_level = 4;
+        status = pjsua_init(&cfg, &log_cfg, NULL);
+        if (status != PJ_SUCCESS) error_exit("Error in pjsua_init()", status);
     }
 
-
-    // Initialize configs with default settings.
-    pjsua_config_default(&ua_cfg);
-    pjsua_logging_config_default(&log_cfg);
-    pjsua_media_config_default(&media_cfg);
-
-    ua_cfg.thread_cnt = 1;   // number of worker threads defaults to 1
-    ua_cfg.max_calls = maxCalls;
-
-    ua_cfg.enable_unsolicited_mwi = 1;  // allow unsolicited on all accounts
-
-
-    // Initialize pjsua
-    status = pjsua_init(&ua_cfg, &log_cfg, &media_cfg);
-    if (status != PJ_SUCCESS) {
-        pjsua_perror("pjApi.c", "Error initializing pjsua", status);
-        return status;
+/* Add UDP transport. */
+    {
+        pjsua_transport_config cfg;
+        pjsua_transport_config_default(&cfg);
+        cfg.port = 5060;
+        status = pjsua_transport_create(PJSIP_TRANSPORT_UDP, &cfg, NULL);
+        if (status != PJ_SUCCESS) error_exit("Error creating transport", status);
     }
 
-    // Create transport mechanisms  -- UDP
-    pjsua_transport_config_default(&transport_cfg);
-    transport_cfg.port = 5070; //This is only temporary for apollo. Need to ensure to use 5060.
-
-
-    status = pjsua_transport_create(PJSIP_TRANSPORT_UDP, &transport_cfg, &udp_id);
-    if (status != PJ_SUCCESS) {
-        pjsua_perror("pjApi.c", "Error creating UDP transport", status);
-        return status;
-    }
-
+/* Initialization is done, now start pjsua */
     status = pjsua_start();
-    if (status != PJ_SUCCESS) {
-        pjsua_destroy();
-        pjsua_perror("pjApi.c", "Error starting pjsua", status);
-        return status;
-    }
+    if (status != PJ_SUCCESS) error_exit("Error starting pjsua", status);
 
-    engineInitialized = 1;
+/* Register to SIP server by creating SIP account. */
+    {
+        pjsua_acc_config cfg;
+        pjsua_acc_config_default(&cfg);
+        cfg.id = pj_str("sip:" SIP_USER "@" SIP_DOMAIN);
+        cfg.reg_uri = pj_str("sip:" SIP_DOMAIN);
+        cfg.cred_count = 1;
+        cfg.cred_info[0].realm = pj_str(SIP_DOMAIN);
+        cfg.cred_info[0].scheme = pj_str("digest");
+        cfg.cred_info[0].username = pj_str(SIP_USER);
+        cfg.cred_info[0].data_type = PJSIP_CRED_DATA_PLAIN_PASSWD;
+        cfg.cred_info[0].data = pj_str(SIP_PASSWD);
+        status = pjsua_acc_add(&cfg, PJ_TRUE, &acc_id);
+        if (status != PJ_SUCCESS) error_exit("Error adding account", status);
+    }
 
     return status;
 }
@@ -97,25 +100,62 @@ JNIEXPORT jint Java_com_freetalk_pjinterface_PJ_destroyEngineJNI(JNIEnv* env, jo
     pj_pool_release(poolJNI);
     poolJNI = NULL;
 
-    engineInitialized = 0;
-
     __android_log_print(ANDROID_LOG_INFO, TAG, "Destroy SIP Engine");
     status = pjsua_destroy();
     return status;
 }
 
-// returns zero if the engine has been initialized.  // Prints error message if not initialized
-int checkEngine(char * pFcnName) {
-    if (engineInitialized == 0) {
-        if (pFcnName != NULL)
-            __android_log_print(ANDROID_LOG_WARN, TAG, "CheckEngine: not initialized: %s", pFcnName);
-        else {
-            __android_log_print(ANDROID_LOG_WARN, TAG, "CheckEngine: not initialized");
-        }
-        return -1000;
-    } else {
-        return 0;
+/* Callback called by the library upon receiving incoming call */
+void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id,
+                             pjsip_rx_data *rdata)
+{
+    pjsua_call_info ci;
+
+    PJ_UNUSED_ARG(acc_id);
+    PJ_UNUSED_ARG(rdata);
+
+    pjsua_call_get_info(call_id, &ci);
+
+    PJ_LOG(3,(TAG, "Incoming call from %.*s!!",
+            (int)ci.remote_info.slen,
+            ci.remote_info.ptr));
+
+    /* Automatically answer incoming calls with 200/OK */
+    pjsua_call_answer(call_id, 200, NULL, NULL);
+}
+
+/* Callback called by the library when call's state has changed */
+void on_call_state(pjsua_call_id call_id, pjsip_event *e)
+{
+    pjsua_call_info ci;
+
+    PJ_UNUSED_ARG(e);
+
+    pjsua_call_get_info(call_id, &ci);
+    PJ_LOG(3,(TAG, "Call %d state=%.*s", call_id,
+            (int)ci.state_text.slen,
+            ci.state_text.ptr));
+}
+
+/* Callback called by the library when call's media state has changed */
+void on_call_media_state(pjsua_call_id call_id)
+{
+    pjsua_call_info ci;
+
+    pjsua_call_get_info(call_id, &ci);
+
+    if (ci.media_status == PJSUA_CALL_MEDIA_ACTIVE) {
+        // When media is active, connect call to sound device.
+        pjsua_conf_connect(ci.conf_slot, 0);
+        pjsua_conf_connect(0, ci.conf_slot);
     }
+}
+
+void error_exit(const char *title, pj_status_t status)
+{
+    pjsua_perror(TAG, title, status);
+    pjsua_destroy();
+    exit(1);
 }
 
 #undef __PJAPI__
